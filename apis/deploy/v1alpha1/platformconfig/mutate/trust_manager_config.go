e8129f7d5730f8eff6258935479e9217a4f8a70a
cpwooley12 <connor.wooley12@gmail.com>
2024-07-22 16:45:05 -0500

    adding the trust manager config

--- apis/deploy/v1alpha1/platformconfig/mutate/trust_manager_config.go
+++ apis/deploy/v1alpha1/platformconfig/mutate/trust_manager_config.go
@@ -17,11 +17,37 @@ limitations under the License.
 package mutate
 
 import (
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"fmt"
 
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	trustmanagerv1alpha1 "github.com/tbd-paas/capabilities-trust-manager-operator/apis/trustmanager/v1alpha1"
 	"github.com/nukleros/operator-builder-tools/pkg/controller/workload"
 
 	deployv1alpha1 "github.com/tbd-paas/platform-config-operator/apis/deploy/v1alpha1"
+)
+
+const (
+	// deployment sizes
+	Small  = "small"
+	Medium = "medium"
+	Large  = "large"
+
+	// controller resource requests, limits and replicas
+	smallControllerReplicas  = 1
+	mediumControllerReplicas = 1
+	largeControllerReplicas  = 2
+	
+	smallControllerCPURequests    = "25m"
+	smallControllerMemoryRequests = "32Mi"
+	smallControllerMemoryLimits   = "64Mi"
+
+	mediumControllerCPURequests    = "50m"
+	mediumControllerMemoryRequests = "64Mi"
+	mediumControllerMemoryLimits   = "96Mi"
+
+	largeControllerCPURequests    = "50m"
+	largeControllerMemoryRequests = "64Mi"
+	largeControllerMemoryLimits   = "96Mi"
 )
 
 // MutateTrustManagerConfig mutates the TrustManager resource with name config.
@@ -35,7 +61,39 @@ func MutateTrustManagerConfig(
 		return []client.Object{original}, nil
 	}
 
-	// mutation logic goes here
+	trustManager, ok := original.(*trustmanagerv1alpha1.TrustManager)
+	if !ok {
+		return nil, fmt.Errorf("original object is not a TrustManager - found  %T", original)
+	}
 
-	return []client.Object{original}, nil
+	//apply the trustmanager configuration
+	if err := applyTrustManagerConfig(trustmanager, parent.Spec.Platform.Identity.DeploymentSize); err != nil {
+		return nil, err
+	}
+
+	return []client.Object{trustManager}, nil
+}
+
+func applyTrustManagerConfig(trustManager *trustmanagerv1alpha1.TrustManager, deploymentSize string) error {
+	switch deploymentSize {
+	case Small:
+		trustManager.Spec.Controller.Replicas = smallControllerReplicas
+		trustManager.Spec.Controller.Resources.Requests.CPU = smallControllerCPURequests
+		trustManager.Spec.Controller.Resources.Requests.Memory = smallControllerMemoryRequests
+		trustManager.Spec.Controller.Resources.Limits.Memory = smallControllerMemoryLimits
+	case Medium:
+		trustManager.Spec.Controller.Replicas = mediumControllerReplicas
+		trustManager.Spec.Controller.Resources.Requests.CPU = mediumControllerCPURequests
+		trustManager.Spec.Controller.Resources.Requests.Memory = mediumControllerMemoryRequests
+		trustManager.Spec.Controller.Resources.Limits.Memory = mediumControllerMemoryLimits
+	case Large:
+		trustManager.Spec.Controller.Replicas = largeControllerReplicas
+		trustManager.Spec.Controller.Resources.Requests.CPU = largeControllerCPURequests
+		trustManager.Spec.Controller.Resources.Requests.Memory = largeControllerMemoryRequests
+		trustManager.Spec.Controller.Resources.Limits.Memory = largeControllerMemoryLimits
+	default:
+		return fmt.Errorf("invalid deployment size %s", deploymentSize)
+	}
+
+	return nil
 }
